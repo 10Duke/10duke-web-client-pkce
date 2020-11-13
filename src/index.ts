88f297fe36f12ec5f363ea14fb38a6586ae778bd
Jarkko Selkäinaho <jarkko@10duke.com>
2020-11-13 09:09:57 +0200

    Prepare for publish

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export * from "./authn/AccessTokenResponse";
+export * from "./authn/Authentication";
+export * from "./authn/Authenticator";
+export * from "./authn/LicenseChecker";
+export * from "./authn/userinfo";
